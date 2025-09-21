cccd4557822164580ad339ef012b2e695ec5603c
Sam Lam <xlin67948gmail.com>
2025-09-21 12:02:50 +0800

    fixed: directly problem

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  }
   // server: {
   //   port: 5174, // 默认 5173
   //   open: true  // 自动打开浏览器
