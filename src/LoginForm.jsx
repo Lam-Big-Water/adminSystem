92463ac5cde90d2645425beec8dcc1e04dbc46ae
Sam Lam <xlin67948gmail.com>
2025-12-19 16:42:14 +0800

    feat(auth): remove Google Sign-In integration
    fix(passwordInput): toggle visibility for all password fields
    
    style(LoginForm): update style

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,19 +1,13 @@
-import React, {useState} from "react";
-import supabase from "./server/supabase"
-import {useLogin} from './query/auth/useLogin'
-import SpinnerMini from './SpinnerMini'
+import React, { useState } from "react";
+import { useLogin } from "./query/auth/useLogin";
+import SpinnerMini from "./SpinnerMini";
+import { LogIn, EyeOff, Eye } from "lucide-react";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("samlam@showcase.com");
   const [password, setPassword] = useState("123123123");
+  const [eye, setEye] = useState(false);
   const { login, isPending } = useLogin();
-
-  const signUp = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-    });
-  };
-
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -31,36 +25,84 @@ const LoginForm = () => {
   }
 
   return (
-    <div className="w-full flex justify-center p-6 mx-6 text-[var(--text-primary)] font-normal">
-      <form onSubmit={handleSubmit} className="w-full max-w-128 flex flex-col p-5 gap-4">
+    <div className="mx-auto w-full flex flex-col justify-center space-y-2 py-8 sm:w-[480px] sm:p-8 text-black dark:text-amber-50">
+      <h1 className="flex justify-center items-center text-xl font-semibold mb-4">
+        ❖ Admin Dashboard
+      </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full flex flex-col gap-4 p-6 shadow-sm rounded-xl border border-neutral-300 dark:border-zinc-700"
+      >
         <div>
-          <h2 className="text-3xl font-black pb-2">Login to your account</h2>
-          <span className="text-[var(--text-second)] text-base">Enter your email below to login to your account</span>
+          <h2 className="text-lg font-semibold">Sign in</h2>
+          <span className="text-neutral-500 dark:text-stone-400 text-sm font-medium">
+            Enter your email and password below to
+            <br />
+            log into your account
+          </span>
         </div>
 
-        <div className="flex flex-col">
-          <label htmlFor="email" className="text-xl font-bold pt-4 pb-2">Email</label>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} id="email" placeholder="m@example.com" className="text-lg px-4 py-3 rounded-md bg-[var(--color-block)] border-[2px] border-[var(--color-border)]" type="email" />
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="email"
+            className="text-sm leading-none font-medium select-none"
+          >
+            Email
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            placeholder="name@example.com"
+            className="h-9 w-full min-w-0 placeholder:text-zinc-400 rounded-md border border-zinc-700 px-3 py-1 text-base shadow-xs transition-[color,box-shadow]"
+            type="email"
+          />
         </div>
 
-        <div className="flex flex-col">
-          <div className="flex font-bold pt-4 pb-2">
-          <label htmlFor="password" className="text-xl">Password</label>
-          <a className="text-base cursor-pointer font-normal ml-auto">Forgot your password?</a>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center">
+            <label
+              htmlFor="password"
+              className="text-sm leading-none font-medium select-none"
+            >
+              Password
+            </label>
+            <a className="ml-auto text-neutral-500 dark:text-stone-400 text-sm font-medium hover:opacity-75 cursor-pointer">
+              Forgot password?
+            </a>
           </div>
-          <input value={password} onChange={(e) => setPassword(e.target.value)} id="password" placeholder="m@example.com" className="text-lg px-4 py-3 rounded-md bg-[var(--color-block)] border-[2px] border-[var(--color-border)]" type="password" />
+          <div className="relative">
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              placeholder="********"
+              className="h-9 w-full min-w-0 placeholder:text-zinc-400 rounded-md border border-zinc-700 px-3 py-1 text-base shadow-xs transition-[color,box-shadow]"
+              type={eye ? "text" : "password"}
+            />
+            <button
+              onClick={() => setEye(!eye)}
+              type="button"
+              className="text-zinc-400 absolute end-2 top-1/2 w-6 h-6 -translate-y-1/2 cursor-pointer hover:text-black dark:hover:text-amber-50"
+            >
+              {eye ? <Eye size="20px" /> : <EyeOff size="20px" />}
+            </button>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-4 pt-4 pb-2">
-          <button className="flex justify-center items-center text-xl font-bold cursor-pointer text-[var(--color-second)] bg-[var(--color-primary)] border-[2px] border-[var(--color-border)] px-4 py-3 rounded-md">{isPending ? <SpinnerMini /> : "Login"}</button>
-        </div>
-
-
+        <button
+          type="submit"
+          className="h-9 px-4 py-2 bg-black text-amber-50 dark:bg-amber-50 dark:text-black inline-flex items-center justify-center whitespace-normal rounded-md text-sm font-medium transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 hover:bg-black/80 dark:hover:bg-amber-50/90"
+        >
+          {isPending ? (
+            <SpinnerMini />
+          ) : (
+            <span className="flex items-center gap-2">
+              <LogIn /> Login
+            </span>
+          )}
+        </button>
       </form>
-
-      <div className="flex flex-col gap-4 pt-4 pb-2">
-          <button onClick={signUp} className="flex justify-center items-center text-xl font-bold cursor-pointer text-[var(--color-second)] bg-[var(--color-primary)] border-[2px] border-[var(--color-border)] px-4 py-3 rounded-md">Google</button>
-        </div>
     </div>
   );
 };
