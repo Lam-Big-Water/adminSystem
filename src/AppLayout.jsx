49e9ecda13130398a0618152021fb0600bd72dc2
Sam Lam <xlin67948gmail.com>
2025-12-25 20:56:17 +0800

    feat(ui): Fix the Outlet layout.

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
 
       <div className="flex gap-2 w-full border border-[var(--color-border)] rounded-2xl">
         <Sidebar />
-        <div className="w-full overflow-y-auto p-6">
+        <div className="w-full p-6 overflow-auto h-screen">
         <Outlet />
         </div>
       </div>
