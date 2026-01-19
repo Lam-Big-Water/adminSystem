3305100118b75393c68f75b6949b960a04d2a274
Sam Lam <xlin67948gmail.com>
2026-01-19 21:55:17 +0800

    refactor: modify icon size, styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   
   return (
-    <div className="w-full flex gap-4 items-center justify-end">
+    <div className="sticky top-0 bg-background/50 p-4 text-foreground backdrop-blur-sm z-50 w-full h-16 flex gap-4 items-center justify-end">
       {/* <button onClick={handleSlide} className="mr-auto sm:hidden">
         <PanelRightOpen />
       </button> */}
@@ -15,16 +15,16 @@ const Header = () => {
         onClick={toggleDarkMode}
         className="p-1 w-10 h-10 flex justify-center items-center rounded-full hover:bg-muted"
       >
-        {isDarkMode ? <Moon /> : <SunMedium />}
+        {isDarkMode ? <Moon size={24} strokeWidth={2}/> : <SunMedium size={24} strokeWidth={2}/>}
       </button>
       <button className="p-1 w-10 h-10 flex justify-center items-center rounded-full hover:bg-muted">
         <Link to="/settings">
-          <Settings />
+          <Settings size={20} strokeWidth={2}/>
         </Link>
       </button>
       <button className="bg-muted p-1 w-10 h-10 flex justify-center items-center rounded-full">
         <Link to="/settings">
-          <span className="text-sm">SL</span>
+          <span className="font-medium">SL</span>
         </Link>
       </button>
     </div>
